74e3bee0e57457a139ded92fa17d04ecf483099d
laumgm <lauramarie.manlapaz@tup.edu.ph>
2020-11-18 19:31:39 +0800

    Sprint 2 w/ User Status

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -24,6 +24,11 @@ const userSchema = mongoose.Schema(
       type: Boolean,
       required: true,
       default: false,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+      required: true,
     }
   },
   {
@@ -35,13 +40,20 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
+userSchema.methods.comparePassword = async function (enteredKey) {
+  return await bcrypt.compare(enteredKey, this.securityQuestion[1]);
+};
+
 userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) {
+   this.securityQuestion[1] = this.securityQuestion[1].toLowerCase();
+
+  if (!this.isModified('password') && !this.isModified('securityQuestion[1]')) {
     next();
   }
 
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
+  this.securityQuestion[1] = await bcrypt.hash(this.securityQuestion[1], salt);
 });
 
 const User = mongoose.model('User', userSchema);
